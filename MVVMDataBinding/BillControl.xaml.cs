449d55f0242364e059a9ccdd1a64d54fc83d1657
Andrew <Andrew@DESKTOP-1UPPPHP>
2020-04-03 22:44:29 -0500

    I have all the code written but I cant get the right pating required

---
+++ MVVMDataBinding/BillControl.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CashRegister;
+
+namespace MVVMDataBinding
+{
+    /// <summary>
+    /// Interaction logic for BillControl.xaml
+    /// </summary>
+    public partial class BillControl : UserControl
+    {
+        public BillControl()
+        {
+            InitializeComponent();
+        }
+        public Bills Denomination
+        {
+            get { return (Bills)GetValue(DenominationProperty); }
+            set { SetValue(DenominationProperty, value); }
+        }
+
+        public static readonly DependencyProperty DenominationProperty =
+       DependencyProperty.Register(
+           "Denomination",                     // The name of the property
+           typeof(Bills),                      // The type of the property
+           typeof(BillControl),                // The property's control
+           new PropertyMetadata(Bills.One)   // The Property Medata
+           );
+
+        public static readonly DependencyProperty QuantityProperty =
+          DependencyProperty.Register(
+              "Quantity",
+              typeof(int),
+              typeof(BillControl),
+              new FrameworkPropertyMetadata(
+                  0,
+                  FrameworkPropertyMetadataOptions.BindsTwoWayByDefault
+              )
+          );
+
+        public int Quantity
+        {
+            get { return (int)GetValue(QuantityProperty); }
+            set { SetValue(QuantityProperty, value); }
+        }
+
+        public void OnAddClicked(object sender, RoutedEventArgs args)
+        {
+            Quantity++;
+        }
+
+        public void OnRemoveClicked(object sender, RoutedEventArgs args)
+        {
+            Quantity--;
+        }
+
+    }
+}
---
+++ MVVMDataBinding/CoinControl.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CashRegister;
+//using MVVMDataBinding;
+
+namespace MVVMDataBinding
+{
+    /// <summary>
+    /// Interaction logic for CoinControl.xaml
+    /// </summary>
+    public partial class CoinControl : UserControl
+    {  
+        public CoinControl()
+        {
+            InitializeComponent();
+        }
+     
+        public Coins Denomination
+        {
+            get { return (Coins)GetValue(DenominationProperty); }
+            set { SetValue(DenominationProperty, value); }
+        }
+
+        public static readonly DependencyProperty DenominationProperty =
+       DependencyProperty.Register(
+           "Denomination",                     // The name of the property
+           typeof(Coins),                      // The type of the property
+           typeof(CoinControl),                // The property's control
+           new PropertyMetadata(Coins.Penny)   // The Property Medata
+           );
+
+        public static readonly DependencyProperty QuantityProperty =
+          DependencyProperty.Register(
+              "Quantity",
+              typeof(int),
+              typeof(CoinControl),
+              new FrameworkPropertyMetadata(
+                  0,
+                  FrameworkPropertyMetadataOptions.BindsTwoWayByDefault
+              )
+          );
+
+        public int Quantity
+        {
+            get { return (int)GetValue(QuantityProperty); }
+            set { SetValue(QuantityProperty, value); }
+        }
+
+        public void OnAddClicked(object sender, RoutedEventArgs args)
+        {
+            Quantity++;
+        }
+
+        public void OnRemoveClicked(object sender, RoutedEventArgs args)
+        {
+            Quantity--;
+        }
+      
+    }
+}
